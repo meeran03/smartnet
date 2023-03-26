1f009908eeebc2ee0ade058018bfd91bd5cf8c07
meeran03 <muhammadmeeran2003@gmail.com>
2023-03-26 22:20:35 +0500

    chore: uncommented code

--- nn.cpp
+++ nn.cpp
@@ -3,65 +3,65 @@
 #include <graphviz/cgraph.h>
 #include "Utils.cpp"
 #include "Neuron.cpp"
-// #include "Layer.cpp"
-// #include "MLP.cpp"
+#include "Layer.cpp"
+#include "MLP.cpp"
 
-// void train(MLP &mlp, vector<vector<double>> &data, vector<double> &labels, int epochs, double learning_rate)
-// {
-//     vector<vector<Value>> preds;
-//     for (int i = 0; i < epochs; i++)
-//     {
-//         preds = {};
-//         Value loss(0.0);
-//         for (int j = 0; j < data.size(); j++)
-//         {
-//             vector<Value> pred = mlp.predict(data[j]);
-//             preds.push_back(pred);
-//             for (Value p : pred)
-//             {
-//                 Value ans = (p - labels[j]) ^ 2;
-//                 ans.setLabel("ans");
-//                 loss = loss + ans;
-//             }
+void train(MLP &mlp, vector<vector<double>> &data, vector<double> &labels, int epochs, double learning_rate)
+{
+    vector<vector<Value>> preds;
+    for (int i = 0; i < epochs; i++)
+    {
+        preds = {};
+        Value loss(0.0);
+        for (int j = 0; j < data.size(); j++)
+        {
+            vector<Value> pred = mlp.predict(data[j]);
+            preds.push_back(pred);
+            for (Value p : pred)
+            {
+                Value ans = (p - labels[j]) ^ 2;
+                ans.setLabel("ans");
+                loss = loss + ans;
+            }
 
-//         }
+        }
 
-//         for (Value *param : mlp.parameters())
-//         {
-//             param->grad = 0;
-//         }
+        for (Value *param : mlp.parameters())
+        {
+            param->grad = 0;
+        }
 
-//         loss._backward();
-//         for (Value *param : mlp.parameters())
-//         {
-//             cout << "Param: " << *param << endl;
-//             cout << "(LEARNING_RATE * param->grad)" << (learning_rate * param->grad) << endl;
-//             param->data = param->data - (learning_rate * param->grad);
-//             cout << "Param: " << *param << endl;
-//         }
-//         cout << "Loss: " << loss.data << endl;
-//     }
-// }
-
-
-// void exampleTrain() {
-//     MLP mlp(3, {4, 4, 1});
-
-//     vector<vector<double>> data = {
-//         {2.0, 3.0, -1.0},
-//         {3.0, -1.0, 0.5},
-//         {0.5, 1.0, 1.0},
-//         {1.0, 1.0, -1.0},
-//     };
+        loss._backward();
+        for (Value *param : mlp.parameters())
+        {
+            cout << "Param: " << *param << endl;
+            cout << "(LEARNING_RATE * param->grad)" << (learning_rate * param->grad) << endl;
+            param->data = param->data - (learning_rate * param->grad);
+            cout << "Param: " << *param << endl;
+        }
+        cout << "Loss: " << loss.data << endl;
+    }
+}
 
 
-//     vector<double> labels = {1.0, -1.0, -1.0, 1.0};
+void exampleTrain() {
+    MLP mlp(3, {4, 4, 1});
 
-//     const int EPOCHS = 1;
-//     const double LEARNING_RATE = 0.01;
+    vector<vector<double>> data = {
+        {2.0, 3.0, -1.0},
+        {3.0, -1.0, 0.5},
+        {0.5, 1.0, 1.0},
+        {1.0, 1.0, -1.0},
+    };
 
-//     train(mlp, data, labels, EPOCHS, LEARNING_RATE);
-// }
+
+    vector<double> labels = {1.0, -1.0, -1.0, 1.0};
+
+    const int EPOCHS = 1;
+    const double LEARNING_RATE = 0.01;
+
+    train(mlp, data, labels, EPOCHS, LEARNING_RATE);
+}
 
 
 int main() {
@@ -70,9 +70,5 @@ int main() {
     rand();
     rand();
 
-    Neuron n(3);
-    vector<double> data = {2.0, 3.0, -1.0};
-    Value res = n.predict(data);
-    res._backward();
-    printDiGraph(&res);
+    exampleTrain();
 }
